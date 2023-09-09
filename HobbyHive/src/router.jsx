ac964517539958662add116dddac6a3f58d824da
SmitAkbari26 <akbarismit26@gmail.com>
2023-09-09 10:52:52 +0530

    Initial

---
+++ HobbyHive/src/router.jsx
@@ -0,0 +1,56 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "../views/Login";
+import SignUp from "../views/Signup";
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signUp",
+        element: <SignUp />,
+    },
+    // {
+    //     path: "/",
+    //     element: <DefaultLayout />,
+    //     children: [
+    //         {
+    //             path: "/dashboard",
+    //             element: <Navigate to="/" />,
+    //         },
+    //         {
+    //             path: "/",
+    //             element: <Dashboard />,
+    //         },
+    //         {
+    //             path: "/surveys",
+    //             element: <Surveys />,
+    //         },
+    //         {
+    //             path: "/surveys/create",
+    //             element: <SurveyView />,
+    //         },
+    //     ],
+    // },
+    // {
+    //     path: "/",
+    //     element: <GuestLayout />,
+    //     children: [
+    //         {
+    //             path: "login",
+    //             element: <Login />,
+    //         },
+    //         {
+    //             path: "signup",
+    //             element: <Signup />,
+    //         },
+    //     ],
+    // },
+]);
+
+export default Router;
